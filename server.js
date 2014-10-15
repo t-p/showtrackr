c2364b9d011aa0e8aa374f92ec80ea3ba94c7a18
Thomas Pfeiffer <thomas@ibpfeiffer.de>
2014-10-15 18:57:04 +0200

    add login and subscribe

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var bcrypt = require('bcryptjs');
 
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
 var async = require('async');
 var request = require('request');
 var xml2js = require('xml2js');
@@ -65,6 +69,31 @@ var Show = mongoose.model('Show', showSchema);
 
 mongoose.connect('localhost');
 
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
+passport.use(new LocalStrategy({ usernameField: 'email' }, function(email, password, done) {
+    User.findOne({ email: email }, function(err, user) {
+        if (err) return done(err);
+        if (!user) return done(null, false);
+        user.comparePassword(password, function(err, isMatch) {
+            if (err) return done(err);
+            if (isMatch) return done(null, user);
+            return done(null, false);
+        });
+    });
+}));
+
+function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) next();
+    else res.send(401);
+}
+
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
@@ -72,7 +101,16 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
+app.use(session({ secret: 'keyboard cat' }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(function(req, res, next) {
+    if (req.user) {
+        res.cookie('user', JSON.stringify(req.user));
+    }
+    next();
+});
 
 app.get('/api/shows', function(req, res, next) {
     var query = Show.find();
@@ -172,6 +210,45 @@ app.post('/api/shows', function(req, res, next) {
       res.send(200);
     });
   });
+});
+app.post('/api/login', passport.authenticate('local'), function(req, res) {
+    res.cookie('user', JSON.stringify(req.user));
+    res.send(req.user);
+});
+app.post('/api/signup', function(req, res, next) {
+    var user = new User({
+        email: req.body.email,
+        password: req.body.password
+    });
+    user.save(function(err) {
+        if (err) return next(err);
+        res.send(200);
+    });
+});
+app.get('/api/logout', function(req, res, next) {
+    req.logout();
+    res.send(200);
+});
+app.post('/api/subscribe', ensureAuthenticated, function(req, res, next) {
+    Show.findById(req.body.showId, function(err, show) {
+        if (err) return next(err);
+        show.subscribers.push(req.user.id);
+        show.save(function(err) {
+            if (err) return next(err);
+            res.send(200);
+        });
+    });
+});
+app.post('/api/unsubscribe', ensureAuthenticated, function(req, res, next) {
+    Show.findById(req.body.showId, function(err, show) {
+        if (err) return next(err);
+        var index = show.subscribers.indexOf(req.user.id);
+        show.subscribers.splice(index, 1);
+        show.save(function(err) {
+            if (err) return next(err);
+            res.send(200);
+        });
+    });
 });
 app.get('*', function(req, res) {
     res.redirect('/#' + req.originalUrl);
