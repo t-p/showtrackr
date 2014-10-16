7239b104c484aad668bc9ea773ed93a45739613a
Thomas Pfeiffer <thomas@ibpfeiffer.de>
2014-10-16 18:00:40 +0200

    add background job for mail sending

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ var session = require('express-session');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
+var agenda = require('agenda')({ db: { address: 'localhost:27017/test' } });
+var sugar = require('sugar');
+var nodemailer = require('nodemailer');
+
 var async = require('async');
 var request = require('request');
 var xml2js = require('xml2js');
@@ -93,6 +97,44 @@ function ensureAuthenticated(req, res, next) {
     if (req.isAuthenticated()) next();
     else res.send(401);
 }
+
+agenda.define('send email alert', function(job, done) {
+  Show.findOne({ name: job.attrs.data }).populate('subscribers').exec(function(err, show) {
+    var emails = show.subscribers.map(function(user) {
+      return user.email;
+    });
+
+    var upcomingEpisode = show.episodes.filter(function(episode) {
+      return new Date(episode.firstAired) > new Date();
+    })[0];
+
+    var smtpTransport = nodemailer.createTransport('SMTP', {
+      service: 'SendGrid',
+      auth: { user: 'hslogin', pass: 'hspassword00' }
+    });
+
+    var mailOptions = {
+      from: 'Fred Foo ✔ <foo@blurdybloop.com>',
+      to: emails.join(','),
+      subject: show.name + ' is starting soon!',
+      text: show.name + ' starts in less than 2 hours on ' + show.network + '.\n\n' +
+        'Episode ' + upcomingEpisode.episodeNumber + ' Overview\n\n' + upcomingEpisode.overview
+    };
+
+    smtpTransport.sendMail(mailOptions, function(error, response) {
+      console.log('Message sent: ' + response.message);
+      smtpTransport.close();
+      done();
+    });
+  });
+});
+agenda.start();
+agenda.on('start', function(job) {
+  console.log("Job %s starting", job.attrs.name);
+});
+agenda.on('complete', function(job) {
+  console.log("Job %s finished", job.attrs.name);
+});
 
 var app = express();
 
@@ -201,12 +243,11 @@ app.post('/api/shows', function(req, res, next) {
   ], function(err, show) {
     if (err) return next(err);
     show.save(function(err) {
-      if (err) {
-        if (err.code == 11000) {
-          return res.send(409, { message: show.name + ' already exists.' });
-        }
-        return next(err);
-      }
+      if (err) return next(err);
+
+      var alertDate = Date.create('Next ' + show.airsDayOfWeek + ' at ' + show.airsTime).rewind({ hour: 2});
+      agenda.schedule(alertDate, 'send email alert', show.name).repeatEvery('1 week');
+
       res.send(200);
     });
   });
