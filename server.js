23bafbf024c9fddc25ff1299704dec282024ce40
Thomas Pfeiffer <thomas@ibpfeiffer.de>
2014-10-21 17:09:43 +0200

    move agenda task to bottom, put mail address into env

--- server.js
+++ server.js
@@ -103,44 +103,6 @@ function ensureAuthenticated(req, res, next) {
     else res.send(401);
 }
 
-agenda.define('send email alert', function(job, done) {
-  Show.findOne({ name: job.attrs.data }).populate('subscribers').exec(function(err, show) {
-    var emails = show.subscribers.map(function(user) {
-      return user.email;
-    });
-
-    var upcomingEpisode = show.episodes.filter(function(episode) {
-      return new Date(episode.firstAired) > new Date();
-    })[0];
-
-    var smtpTransport = nodemailer.createTransport('SMTP', {
-      service: 'SendGrid',
-      auth: { user: 'hslogin', pass: 'hspassword00' }
-    });
-
-    var mailOptions = {
-      from: 'Fred Foo ✔ <foo@blurdybloop.com>',
-      to: emails.join(','),
-      subject: show.name + ' is starting soon!',
-      text: show.name + ' starts in less than 2 hours on ' + show.network + '.\n\n' +
-        'Episode ' + upcomingEpisode.episodeNumber + ' Overview\n\n' + upcomingEpisode.overview
-    };
-
-    smtpTransport.sendMail(mailOptions, function(error, response) {
-      console.log('Message sent: ' + response.message);
-      smtpTransport.close();
-      done();
-    });
-  });
-});
-agenda.start();
-agenda.on('start', function(job) {
-  console.log("Job %s starting", job.attrs.name);
-});
-agenda.on('complete', function(job) {
-  console.log("Job %s finished", job.attrs.name);
-});
-
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
@@ -307,4 +269,42 @@ app.use(function(err, req, res, next) {
 });
 app.listen(app.get('port'), function() {
     console.log('Express server listening on port ' + app.get('port'));
+});
+
+agenda.define('send email alert', function(job, done) {
+  Show.findOne({ name: job.attrs.data }).populate('subscribers').exec(function(err, show) {
+    var emails = show.subscribers.map(function(user) {
+      return user.email;
+    });
+
+    var upcomingEpisode = show.episodes.filter(function(episode) {
+      return new Date(episode.firstAired) > new Date();
+    })[0];
+
+    var smtpTransport = nodemailer.createTransport('SMTP', {
+      service: 'SendGrid',
+      auth: { user: 'hslogin', pass: 'hspassword00' }
+    });
+
+    var mailOptions = {
+      from: process.env.SEND_FROM,
+      to: emails.join(','),
+      subject: show.name + ' is starting soon!',
+      text: show.name + ' starts in less than 2 hours on ' + show.network + '.\n\n' +
+        'Episode ' + upcomingEpisode.episodeNumber + ' Overview\n\n' + upcomingEpisode.overview
+    };
+
+    smtpTransport.sendMail(mailOptions, function(error, response) {
+      console.log('Message sent: ' + response.message);
+      smtpTransport.close();
+      done();
+    });
+  });
+});
+agenda.start();
+agenda.on('start', function(job) {
+  console.log("Job %s starting", job.attrs.name);
+});
+agenda.on('complete', function(job) {
+  console.log("Job %s finished", job.attrs.name);
 });
